097a712ff0d66fd02bdbafbb92584e9e7f022855
ganesh701072 <126324113+ganesh701072@users.noreply.github.com>
2025-08-17 03:05:11 +0530

    Add files via upload

---
+++ LogicalOperator.java
@@ -0,0 +1,21 @@
+package org.test;
+
+public class LogicalOperator {
+	public static void main(String[] args) {
+		//or
+		int a=4; int b=80;
+		if (a>=5 || b>=70) {
+			System.out.println("fullticket");
+		}else {
+			System.out.println("half ticket");
+		}
+		//and
+		int c=4004; int d=0;
+		if (c==4004 && d==4004) {
+			System.out.println("give laddoo");
+		}else {
+			System.out.println("tell no laddoo");
+		}
+	}
+
+}
