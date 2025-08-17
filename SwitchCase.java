5998116e463de6390459a0434c3214517e9ae4f9
ganesh701072 <126324113+ganesh701072@users.noreply.github.com>
2025-08-17 15:05:29 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,30 @@
+package launch;
+
+public class SwitchCase {
+	
+	public static void main(String[] args) {
+		int choice =3;
+		switch (choice){
+		case 1:
+			System.out.println("one");
+			break;
+		case 2:
+			System.out.println("two");
+			break;
+		case 3:
+			System.out.println("three");
+			break;
+		case 4:
+			System.out.println("four");
+			break;
+		default:
+			System.out.println("default");
+			break;
+			
+		}
+		
+	}
+
+	
+
+}
